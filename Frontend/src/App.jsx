c2e0042a142be9380303471a16322b9f03d34b8d
Isha12D <ishadoifode6@gmail.com>
2024-09-04 22:38:04 +0530

    updated tailwind content

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,11 @@
-
+import React from 'react';
 
 function App() {
   
-
   return (
-    <>
-      
-    </>
+    <div className='h-screen w-full bg-slate-900'>
+      <h1 className='text-3xl text-white'>Welcome to Shim Services!</h1>
+    </div>
   )
 }
 
