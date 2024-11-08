f078d167cfcb04e59e4cb697d45a8739bb4c7863
Shruti Chaurasia <shrutichaurasia39@gmail.com>
2024-11-08 14:55:09 +0530

    booking form changes

--- Backend/models/booking.js
+++ Backend/models/booking.js
@@ -27,7 +27,7 @@ import connection from '../db/connection.js';
 
 export const getBookingsByServiceProvider = (email, callback) => {
   const query = `
-  SELECT b.* 
+  SELECT distinct b.* 
   FROM booking b
   JOIN sp_services s ON  b.Service_Name = s.Service_Name
   WHERE b.SP_Email = ? OR b.Book_Status = "Pending" OR b.Book_Status="Accepted"
